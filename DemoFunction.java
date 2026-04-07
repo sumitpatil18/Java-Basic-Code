e4771fe19bee538ae1cdf06ab3e58017bf552ebd
Sumit Patil <sumitpatil17020@gmail.com>
2026-04-07 23:47:06 +0530

    Add files via upload

---
+++ DemoFunction.java
@@ -0,0 +1,10 @@
+class DemoFunction{
+    	public int  display(int a,int b){
+        	return a+=b;
+
+    	}
+    	public static void main(String args[]){
+        	DemoFunction d1=new DemoFunction();
+        	System.out.println (d1.display(10,15));
+    	}
+}
