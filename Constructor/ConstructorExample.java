b9773a5be422cad53915cc5eb3b090401d1bd627
Sumit Patil <sumitpatil17020@gmail.com>
2026-04-07 19:38:13 +0530

    Add files via upload

--- Constructor/ConstructorExample.java
+++ Constructor/ConstructorExample.java
@@ -1,1 +1,33 @@
-
+class ConstructorExample{  
+    	int a,b,c;
+	//Default constructor
+	public  ConstructorExample(){
+        	a=70;b=80;c=90;
+	}
+	//Parameterised constructor
+   	public ConstructorExample(int x, int y,int z){
+        	a=x;
+        	b=y;
+    	    	c=z;
+    	}
+	//Copy constructor
+    	public ConstructorExample(ConstructorExample c){
+        	a=c.a;
+        	b=c.b;
+        	c=c.c;
+        	System.out.println("Copy constructor call...");
+    	}
+    	public void show(){
+        	System.out.println("Value of a is:"+a);
+        	System.out.println("Value of b is:"+b);
+        	System.out.println("Value of c is:"+c);
+        }
+    public static void main(String args[]){
+        ConstructorExample d=new ConstructorExample();
+        d.show();
+        ConstructorExample d1=new ConstructorExample(10,20,30);
+        d1.show();
+        ConstructorExample d2=new ConstructorExample(d1);
+        d2.show();
+    }
+}
