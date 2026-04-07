576f5d19310dcac2a9d548b32c5428d9e0f99295
Sumit Patil <sumitpatil17020@gmail.com>
2026-04-07 20:56:58 +0530

    Update MultiLevelInheritance.java

--- Inheritance/MultiLevelInheritance.java
+++ Inheritance/MultiLevelInheritance.java
@@ -1,1 +1,22 @@
+class A {
+	void funA(){
+		int a = 100;
+		System.out.println(" Value Of a:"+a);
+	}
+}
 
+class B extends A {
+	void funB(){
+		int a = 200;
+		System.out.println(" Value Of b:"+a);
+	}
+}
+
+class MultiLevelInheritance extends B{
+    public static void main(String[] var){
+	MultiLevelInheritance mli = new MultiLevelInheritance();
+	mli.funA();
+	mli.funB();
+    }
+}
+   
